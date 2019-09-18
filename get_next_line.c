ae710ceb2918e01cfb2e90f39434b018c748f619
Samuro Norcross <snorcros@ga-q3.21-school.ru>
2019-09-18 20:19:32 +0300

    work

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,14 @@
 /*   By: snorcros <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 10:53:12 by snorcros          #+#    #+#             */
-/*   Updated: 2019/09/16 21:21:37 by snorcros         ###   ########.fr       */
+/*   Updated: 2019/09/18 20:15:41 by snorcros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 //#include <stdio.h>
 //#include <fcntl.h>
+
 static char	*ft_del_line(char *s)
 {
 	char	*buf;
@@ -25,6 +26,7 @@ static char	*ft_del_line(char *s)
 	if (buf)
 	{
 		free(s);
+		s = NULL;
 		return (buf);
 	}
 	return (NULL);
@@ -43,7 +45,7 @@ static char	*ft_first_str(char *dst)
 	buf = ft_strnew(i);
 	if (!buf)
 		return (NULL);
-	ft_strlcat(buf, dst, i);
+	ft_strncpy(buf, dst, i);
 	return (buf);
 }
 
@@ -51,22 +53,22 @@ int			get_next_line(const int fd, char **line)
 {
 	int			rd;
 	static char	*descriptors[10240];
-	char		buf[BUFF_SIZE];
+	char		buf[BUFF_SIZE + 1];
 
 	if (fd < 0)
 		return (-1);
+	buf[BUFF_SIZE] = '\0';
 	if (!descriptors[fd]) 
-		descriptors[fd] = ft_strnew(0);
-	rd = read(fd, buf, BUFF_SIZE);
-	if (rd <= 0)
-		return (-1);
-	while (!ft_strchr(descriptors[fd], '\n'))
-	{
+		descriptors[fd] = ft_strnew(1);
+	while (!ft_strchr(descriptors[fd], '\n')
+			&& (rd = read(fd, buf, BUFF_SIZE)) != 0)
+ 	{
+		if (rd < 0)
+			return (-1);
 		descriptors[fd] = ft_strjoin(descriptors[fd], buf);
-		rd = read(fd, buf, BUFF_SIZE);
 	}
 	*line = ft_first_str(descriptors[fd]);
-	if (!*line)
+	if (!*line && !rd)
 	{
 		free(descriptors[fd]);
 		return (0);
@@ -74,20 +76,65 @@ int			get_next_line(const int fd, char **line)
 	descriptors[fd] = ft_del_line(descriptors[fd]);
 	return (1);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 int		main(void)
 {
-	int		fd;
+//	printf("\nFirst Descriptors %s \n", descriptors[fd]);
+//	printf("\n\nNow i read: %s\n\n", buf);
+// printf("%s", *line);
+//	printf("\nOut Descriptors %s \n", descriptors[fd]);
+	int		fd1, fd2, fd;
 	int		i;
 	char	*arr = NULL;
 
-	fd = open("test", O_RDONLY);
-	i = get_next_line(fd, &arr);
-	printf("Int %d\n", i);
-	printf("%s", arr);
-//	i = get_next_line(fd, &arr);
-//	printf("Int %d\n", i);
-//	printf("%s", arr);
+	i = 1;
+	fd1 = open("f1", O_RDONLY);
+	fd2 = open("f2", O_RDONLY);
+		i = get_next_line(fd1, &arr);
+//		printf("\n\nInt %d\n", i);
+		printf("%s\n\n", arr);
+		i = get_next_line(fd2, &arr);
+//		printf("\n\nInt %d\n", i);
+		printf("%s\n", arr);
+		i = get_next_line(fd1, &arr);
+//		printf("\n\nInt %d\n", i);
+		printf("%s\n", arr);
+
+		i = get_next_line(fd2, &arr);
+//		printf("\n\nInt %d\n", i);
+		printf("%s\n\n", arr);
+		i = get_next_line(fd1, &arr);
+//		printf("\n\nInt %d\n", i);
+		printf("%s\n", arr);
+		i = get_next_line(fd2, &arr);
+//		printf("\n\nInt %d\n", i);
+		printf("%s\n\n", arr);
+
+	fd = open("Libft/ft_strjoin.c", O_RDONLY);
+	while ((i = get_next_line(fd, &arr)) != 0)
+	{
+//		printf("Int %d\n", i);
+		printf("%s\n", arr);
+//		printf("\n");
+	}
+	
+	close(fd1);
+	close(fd2);
 	close(fd);
 	return (0);
-}*/
+}
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,16 @@
 /*   By: snorcros <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/16 10:54:04 by snorcros          #+#    #+#             */
-/*   Updated: 2019/09/16 21:20:44 by snorcros         ###   ########.fr       */
+/*   Updated: 2019/09/18 19:07:59 by snorcros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include "../Libft/libft.h"
+# include "./libft/includes/libft.h"
 
-# define BUFF_SIZE 1000
+# define BUFF_SIZE 500
 # define FD_MAX    10240
-
 
 int		get_next_line(const int fd, char **line);
 
